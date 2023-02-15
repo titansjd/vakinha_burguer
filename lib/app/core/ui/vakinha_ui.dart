529170ec3a6ad53ec882710f6f0546d1bd845262
Diego <titansjd@gmail.com>
2023-02-15 19:53:07 -0300

    comit parcial aula 01

---
+++ lib/app/core/ui/vakinha_ui.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class VakinhaUi {
+  VakinhaUi._();
+  static final ThemeData theme = ThemeData(
+    primaryColor: const Color(0xFF007D21),
+    primaryColorDark: const Color(0xFF0034F),
+  );
+}
